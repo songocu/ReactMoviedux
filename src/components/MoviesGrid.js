7b51e22dd5058e74b23a19da2609aadeb1108272
songocu <songocu_tud@yahoo.com>
2025-05-27 06:52:21 +0300

    Section 5 - Filtering with advanced UI and events

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,6 +5,9 @@ import MovieCard from "./MovieCard";
 export default function MoviesGrid() {
     const [movies, setMouvies] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+
+    const [genre, setGenre] = useState("All Genres");
+    const [rating, setRating] = useState("All");
     
     useEffect ( () => {
         fetch("movies.json") 
@@ -16,8 +19,40 @@ export default function MoviesGrid() {
         setSearchTerm(e.target.value);
     }
 
+    const handleGenreSearch = (e) => {
+        setGenre(e.target.value);
+    }
+
+    const handleRatingSearch = (e) => {
+        setRating(e.target.value);
+    }
+
+    const matchesGenre = (movie, genre) => {
+        return genre === "All Genres" || movie.genre.toLowerCase() === genre.toLowerCase();
+    }
+    const matchesSearchTerm = (movie, searchTerm) => {
+        return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+
+    const matchesRating = (movie, rating) => {
+        switch (rating) {
+            case "All":
+                return true;
+            case 'Good':
+                return movie.rating >=8;
+            case 'Ok':
+                return movie.rating >=5 && movie.rating <8;
+            case 'Bad':
+                return movie.rating < 5;
+            default:
+                return false;    
+        }
+    }
+
     const filteredMovies = movies.filter(movie =>
-        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+        matchesGenre(movie, genre) &&
+        matchesRating(movie, rating) &&
+        matchesSearchTerm(movie, searchTerm)
     );
 
     return (
@@ -28,6 +63,28 @@ export default function MoviesGrid() {
                     placeholder="Search movies..."
                     value={searchTerm}
                     onChange={hadleSearchChange}/>
+
+                <div className="filter-bar">
+                    <div className="filter-slot">
+                        <label>Genre</label>
+                        <select className="filter-dropdown" value={genre} onChange={handleGenreSearch}>
+                            <option>All Genres</option>
+                            <option>Action</option>
+                            <option>Drama</option>
+                            <option>Fantasy</option>
+                            <option>Horror</option>
+                        </select>
+                    </div>
+                    <div className="filter-slot">
+                        <label>Rating</label>
+                        <select className="filter-dropdown" value={rating} onChange={handleRatingSearch}>
+                            <option>All</option>
+                            <option>Good</option>
+                            <option>Ok</option>
+                            <option>Bad</option>
+                        </select>
+                    </div>
+                </div>
             </div>
             <div className="movies-grid">
                 {filteredMovies.map(movie => (
